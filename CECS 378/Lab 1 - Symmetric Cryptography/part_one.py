9cef19cfdc41c7b0f2c768ca49fa3d5b982bcc6d
Giovanni Salas <giosalas25@gmail.com>
2020-09-28 18:35:48 -0700

    completed part two

---
+++ CECS 378/Lab 1 - Symmetric Cryptography/part_one.py
@@ -0,0 +1,56 @@
+import collections
+from collections import Counter
+
+# returns the key for a value
+def get_key(val, dictionary): 
+    for key, value in dictionary.items(): 
+         if val == value: 
+             return key 
+
+def decrypt(phrase, phrases):
+    # alphabet and their numeric values
+    letter_values = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7, 
+    "i": 8, "j": 9, "k": 10, "l": 11, "m": 12, "n": 13, "o": 14, "p": 15, "q": 16, 
+    "r": 17, "s": 18, "t": 19, "u": 20, "v": 21, "w": 22, "x": 23, "y": 24, "z": 25}
+
+    original_phrase = phrase
+    print("Encrypted phrase:", phrase)
+    phrase = phrase.replace(' ', '') # get rid of spaces
+    most_common_letter = collections.Counter(phrase).most_common(1)[0] # find the most common letter
+
+    # assume that the most frequent letter is the encrypted letter for 'e'
+    shift = 0
+    if original_phrase == phrases[0]: 
+        shift = letter_values[most_common_letter[0]] - letter_values['a']
+    elif original_phrase == phrases[1]:
+         shift = letter_values[most_common_letter[0]] - letter_values['t']
+        # 1) most common letter: j; 'a'
+            # most common vowel: 
+        # 2) most common letter: o, 't'
+        # 3) most common letter: t; 't' - not like the first two (sherlock)
+        # 4) most commom letter: q; 'e'
+
+    decrypted_phrase = ""
+    for character in phrase: # get the corresponding letter for each letter in the encrypted phrase
+        decrypted_letter_value = (letter_values[character] - shift) % 26 # substitution cipher
+        decrypted_letter = get_key(decrypted_letter_value, letter_values) # get the new corresponding letter for the value from the line above
+        decrypted_phrase += decrypted_letter # add the letter to the new phrase
+
+    decrypted_phrase = ' '.join(decrypted_phrase[i:i+5] for i in range(0, len(decrypted_phrase), 5)) # re-adding spaces
+    print("Decrypted phrase:", decrypted_phrase)
+
+def main():
+    phrases = []
+    phrase_one = "fqjcb rwjwj vnjax bnkhj whxcq nawjv nfxdu mbvnu ujbbf nnc"
+    phrase_two = "oczmz vmzor jocdi bnojv dhvod igdaz admno ojbzo rcvot jprvi oviyv aozmo cvooj ziejt dojig toczr dnzno jahvi fdiyv xcdzq zoczn zxjiy"
+    phrase_three = "ejitp spawa qleji taiul rtwll rflrl laoat wsqqj atgac kthls iraoa twlpl qjatw jufrh lhuts qataq itats aittk stqfj cae"
+    phrase_four = "iyhqz ewqin azqej shayz niqbe aheum hnmnj jaqii yuexq ayqkn jbeuq iihed yzhni ifnun sayiz yudhe sqshu qesqa iluym qkque aqaqm oejjs hqzyu jdzqa diesh niznj jayzy uiqhq vayzq shsnj jejjz nshna hnmyt isnae sqfun dqzew qiead zevqi zhnjq shqze udqai jrmtq uishq ifnun siiqa suoij qqfni syyle iszhn bhmei squih nimnx hsead shqmr udquq uaqeu iisqe jshnj oihyy snaxs hqihe lsilu ymhni tyz"
+    phrases.append(phrase_one)
+    phrases.append(phrase_two)
+    phrases.append(phrase_three)
+    phrases.append(phrase_four)
+
+    decrypt(phrase_one, phrases)
+    decrypt(phrase_two, phrases)
+
+main()
---
+++ CECS 378/Lab 1 - Symmetric Cryptography/part_two.py
@@ -0,0 +1,63 @@
+from random import shuffle
+
+#def shuffle_word(word):
+#    word = list(word)
+#    shuffle(word)
+#    return ''.join(word)
+
+def encrypt(phrase, key):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    alphabet_key = dict(zip(alphabet, key))
+
+    encrypted_phrase = ""
+    for letter in phrase: # iterate through the given phrase
+        if letter.lower() in alphabet: # encrypt if it's a letter
+            if letter.isupper(): # if the letter in the phrase is uppercase, keep the uppercase
+                encrypted_phrase += alphabet_key[letter.lower()].upper()
+            else: # if lowercase, add as lowercase
+                encrypted_phrase += alphabet_key[letter.lower()]
+        else: # add any spaces or punctuation marks
+            encrypted_phrase += letter
+    
+    print("ORIGINAL PHRASE:", phrase)
+    print("ENCRYPTED PHRASE:", encrypted_phrase)
+    print()
+
+    return encrypted_phrase
+
+def decrypt(phrase, key):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    alphabet_key = dict(zip(key, alphabet))
+
+    decrypted_phrase = ""
+    for letter in phrase: # iterate through the given phrase
+        if letter.lower() in key: 
+            if letter.isupper(): # if the letter in the phrase is uppercase, keep the uppercase
+                decrypted_phrase += alphabet_key[letter.lower()].upper()
+            else: # if lowercase, add as lowercase
+                decrypted_phrase += alphabet_key[letter.lower()]
+        else: # add any spaces or punctuation marks
+            decrypted_phrase += letter
+    
+    print("ENCRYPTED PHRASE:", phrase)
+    print("DECRYPTED PHRASE:", decrypted_phrase)
+    print()
+
+def main():
+    # given phrases to encrypt
+    phrase_one = "He who fights with monsters should look to it that he himself does not become a monster. And if you gaze long into an abyss, the abyss also gazes into you."
+    phrase_two = "There is a theory which states that if ever anybody discovers exactly what the Universe is for and why it is here, it will instantly disappear and be replaced by something even more bizarre and inexplicable. There is another theory which states that this has already happened."
+    phrase_three = "Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off - then, I account it high time to get to sea as soon as I can."
+
+    key = "mudkvecytjohxabrfzplnwgiqs"
+    encrypted_phrase_one = encrypt(phrase_one, key)
+    encrypted_phrase_two = encrypt(phrase_two, key)
+    encrypted_phrase_three = encrypt(phrase_three, key)
+    
+    print("\n")
+
+    decrypt(encrypted_phrase_one, key)
+    decrypt(encrypted_phrase_two, key)
+    decrypt(encrypted_phrase_three, key)
+
+main()
